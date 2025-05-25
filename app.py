ccb7f1c20dcb629cb3a366938618d4972a720b52
yog <yogdeepsingh5105@gmail.com>
2025-05-25 17:35:51 -0300

    inital commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/secure')
+@app.route('/identity')
 def secure():
     return render_template('secure.html')
 
