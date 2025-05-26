726ad8198f9a00f8dd4812cd8a02515898e85d5d
yog <yogdeepsingh5105@gmail.com>
2025-05-26 19:52:55 -0300

    adding key vault info

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, redirect, url_for
-
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 app = Flask(__name__)
+
+key_vault_url = "https://identity-solution.vault.azure.net/"
+credential = DefaultAzureCredential()
+secure_client = SecretClient(vault_url=key_vault_url, credential=credential)
 
 @app.route('/')
 def home():
@@ -8,7 +13,11 @@ def home():
 
 @app.route('/identity')
 def secure():
-    return render_template('secure.html')
+    try:
+        secret = secure_client.get_secret("Azure-identity-key").value 
+    except Exception as e:
+        secret = f"Error retrieving secret: {str(e)}"
+    return render_template('secure.html', secret=secret)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
